088533b4a6ddf888efa078b4258aba3769c14974
PJunhyuk-mac <jgravity@naver.com>
2017-10-30 14:44:23 +0900

    Add Splash page

---
+++ app/src/main/java/network/virus/todawn_android/SplashActivity.java
@@ -0,0 +1,24 @@
+package network.virus.todawn_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                finish();
+            }
+        };
+
+        handler.sendEmptyMessageDelayed(0, 3000);
+    }
+}
